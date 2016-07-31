a63ab7f59d2e2993cdf9aaa96482535b0ca6bd51
ITACHI <2307735634@qq.com>
2016-07-31 15:59:32 +0800

    initial commit

---
+++ QDaily/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  QDaily
+//
+//  Created by ZoeyWang on 16/7/9.
+//  Copyright © 2016年 Zoey. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+//MARK: - life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1)
+    }
+
+}
