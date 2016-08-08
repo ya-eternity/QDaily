4a56724f12aadddb2ac7f0534356dfaa2549c91f
ya-eternity <yeziyan825@gmail.com>
2016-08-08 17:37:54 +0800

    初试数据请求解析

---
+++ QDaily/HomeModel.swift
@@ -0,0 +1,66 @@
+//
+//  HomeModel.swift
+//  QDaily
+//
+//  Created by bmxd-002 on 16/8/8.
+//  Copyright © 2016年 Zoey. All rights reserved.
+//
+
+import UIKit
+
+class HomeModel: NSObject {
+    
+    var image: String?
+    var type: Int?
+    var id: String?
+    var genre: Int?
+    var title: String?
+    var desc: String?
+    var publishTime: String?
+    var commentCount: Int?
+    var praiseCount: Int?
+    var superTag: String?
+    var pageStyle: Int?
+    var postId: String?
+    var appview: String?
+    var datatype: String?
+    var categoryId: Int?
+    var categoryTitle: String?
+    var categoryImage: String?
+    
+    convenience init(dictionary: NSDictionary) {
+        self.init()
+        self.image = dictionary["image"] as? String
+        let dict = dictionary["post"] as! NSDictionary
+        self.type = dict["type"] as? Int
+        self.id = dict["id"] as? String
+        self.genre = dict["genre"] as? Int
+        self.title = dict["title"] as? String
+        self.desc = dict["description"] as? String
+        self.publishTime = dict["publish_time"] as? String
+        self.commentCount = dict["comment_count"] as? Int
+        self.praiseCount = dict["praise_count"] as? Int
+        self.superTag = dict["super_tag"] as? String
+        self.pageStyle = dict["page_style"] as? Int
+        self.postId = dict["post_id"] as? String
+        self.appview = dict["appview"] as? String
+        self.datatype = dict["datatype"] as? String
+        self.categoryId = dict["category"]!["id"] as? Int
+        self.categoryTitle = dict["category"]!["title"] as? String
+        self.categoryImage = dict["category"]!["normal"] as? String
+    }
+    
+    static func modelArray(fromArray fromArray: Array<AnyObject>?) -> Array<HomeModel>? {
+        guard let array = fromArray else {
+            return nil;
+        }
+        var models: Array<HomeModel>? = Array()
+        for dic in array {
+            let model = HomeModel(dictionary: dic as! NSDictionary)
+            models?.append(model)
+        }
+        
+        return models;
+    }
+    
+}
--- QDaily/MainViewController.swift
+++ QDaily/MainViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
+
+let HOME_URL = "http://app3.qdaily.com/app3/homes/index/0.json?"
 
 class MainViewController: UIViewController {
+    
+    var banners: Array<HomeModel>?
+    
+    var feeds: Array<HomeModel>?
+    
     
 //MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1)
+        
+        self.getData()
+    
+    }
+    
+    func getData() {
+        Alamofire.request(.GET, HOME_URL).responseJSON { (response) in
+            guard response.result.isSuccess else {
+                print("request error! \(response.result)")
+                return
+            }
+            let jsonDict = JSON(data: response.data!)
+            print(jsonDict)
+            self.banners = HomeModel.modelArray(fromArray: jsonDict["response"]["banners"].arrayObject)
+            print(self.banners)
+        }
     }
 
 }
